91d08620e6120f88906f08edfc37284f87a908ec
dmqq <kozakdamian73@gmail.com>
2022-02-10 10:45:21 +0100

    feat: Responsive grid layout & empty pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Product from "@modules/Product";
 export default function Home() {
   return (
     <main className="w-full min-h-screen flex flex-col items-center">
-      <div className="p-2 flex flex-wrap justify-center max-w-screen-2xl">
+      <div className="p-2 w-full grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4 xl:w-2/3">
         {new Array(10).fill({}).map((_, i) => (
           <Product key={i.toString()} />
         ))}
