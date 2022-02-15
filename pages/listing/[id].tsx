f5f58611efbb97a72a6c342d469cae8cd9f67fa1
dmqq <kozakdamian73@gmail.com>
2022-02-15 10:35:09 +0100

    fix: Fixed page scaling

--- pages/listing/[id].tsx
+++ pages/listing/[id].tsx
@@ -1,4 +1,5 @@
 import { NextPageContext } from "next";
+import Head from "next/head";
 
 const url =
   "https://i.kym-cdn.com/entries/icons/mobile/000/025/382/Screen_Shot_2018-02-06_at_3.37.14_PM.jpg";
@@ -26,18 +27,17 @@ export async function getServerSideProps({ query }: NextPageContext) {
 export default function Listing({ data }: any) {
   return (
     <main className="w-full flex flex-col items-center">
-      <section className="flex flex-col xl:w-2/4 sm:w-3/4">
-        <article className="p-2 ">
-          <img
-            className="w-full rounded"
-            src="https://i.kym-cdn.com/entries/icons/mobile/000/025/382/Screen_Shot_2018-02-06_at_3.37.14_PM.jpg"
-            alt=""
-          />
-          <div className="pt-2 flex overflow-scroll sm:overflow-hidden h-24 mb-2">
-            <img className="pr-2 rounded-sm last:pr-0" src={url} alt="" />
-            <img className="pr-2 rounded-sm last:pr-0" src={url} alt="" />
-            <img className="pr-2 rounded-sm last:pr-0" src={url} alt="" />
-            <img className="pr-2 rounded-sm last:pr-0" src={url} alt="" />
+      <Head>
+        <title>{data.title}</title>
+      </Head>
+      <section className="flex flex-col w-full xl:w-2/4 sm:w-3/4">
+        <article className="p-2 mt-5">
+          <img className="w-full rounded" src={url} alt="" />
+          <div className="pt-2 flex overflow-scroll sm:overflow-hidden h-16 sm:h-24 mb-2">
+            <img className="pr-2 rounded last:pr-0" src={url} alt="" />
+            <img className="pr-2 rounded last:pr-0" src={url} alt="" />
+            <img className="pr-2 rounded last:pr-0" src={url} alt="" />
+            <img className="pr-2 rounded last:pr-0" src={url} alt="" />
           </div>
         </article>
         <article className="p-2">
@@ -48,7 +48,7 @@ export default function Listing({ data }: any) {
           <h2 className="text-gray-300 mt-2 text-xl font-bold">
             Price: ${data.price / 100}
           </h2>
-          <div className="mt-4 flex overflow-hidden">
+          <div className="mt-4 flex flex-row overflow-hidden">
             <span className="p-2 rounded border border-white text-white mr-2">
               Electronic
             </span>
