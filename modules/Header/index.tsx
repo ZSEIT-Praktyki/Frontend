57989b7555504a53b93dd375b5c6dcf6b047465d
czarny-rycerz <41902732+czarny-rycerz@users.noreply.github.com>
2022-02-21 12:33:32 +0100

    change colors

--- modules/Header/index.tsx
+++ modules/Header/index.tsx
@@ -1,70 +1,70 @@
-import { Button, Input } from "@components/index";
-import { useSelector } from "@utils/store/store";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import { Button, Input } from '@components/index'
+import { useSelector } from '@utils/store/store'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 import {
   AiOutlineUser,
   AiOutlineShoppingCart,
   AiOutlineSearch,
-} from "react-icons/ai";
+} from 'react-icons/ai'
 
 export default function Header() {
-  const router = useRouter();
+  const router = useRouter()
 
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('')
 
   function onSearch() {
-    router.push("/search", {
-      pathname: "/search",
+    router.push('/search', {
+      pathname: '/search',
       query: {
         q: query,
       },
-    });
+    })
   }
 
-  const { isLoggedIn } = useSelector((state) => state.user);
+  const { isLoggedIn } = useSelector((state) => state.user)
 
   return (
-    <header className="w-full p-2 flex flex-row bg-gray-900 justify-between items-center border-b-2 border-gray-800 sticky top-0 z-10">
-      <p className="p-2 font-bold text-purple-800 text-2xl hidden sm:flex w-1/6">
-        <Link href={"/"}>LOGO</Link>
+    <header className='w-full p-2 flex flex-row bg-gray-900 justify-between items-center border-b-2 border-gray-800 sticky top-0 z-10'>
+      <p className='p-2 font-bold !text-rose-600 text-2xl hidden sm:flex w-1/6'>
+        <Link href={'/'}>LOGO</Link>
       </p>
 
-      <div className="w-full sm:w-4/6 justify-center flex m-0">
+      <div className='w-full sm:w-4/6 justify-center flex m-0'>
         <Input
           value={query}
           onChange={({ target }) => setQuery(target.value)}
-          classes="bg-gray-800 w-3/4"
-          placeholder="Search anything, we may have it"
+          classes='bg-gray-800 w-3/4'
+          placeholder='Search anything, we may have it'
         />
         <Button
-          variants="text"
-          classes="border-gray-800 bg-gray-800 hover:bg-gray-900 m-2"
+          variants='text'
+          classes='border-gray-800 bg-gray-800 hover:bg-gray-900 m-2'
           onClick={onSearch}
         >
-          <AiOutlineSearch color="white" />
+          <AiOutlineSearch color='white' />
         </Button>
       </div>
 
       {isLoggedIn && (
-        <section className="hidden flex-row sm:flex w-1/6">
+        <section className='hidden flex-row sm:flex w-1/6'>
           <Button
-            variants="text"
-            classes="font-medium flex flex-row border-gray-800 bg-gray-800 hover:bg-gray-900 px-2"
-            onClick={() => router.push("/user/account")}
+            variants='text'
+            classes='font-medium flex flex-row border-gray-800 bg-gray-800 hover:bg-gray-900 px-2'
+            onClick={() => router.push('/user/account')}
           >
-            <AiOutlineUser color="white" size={20} />
+            <AiOutlineUser color='white' size={20} />
           </Button>
           <Button
-            variants="text"
-            classes="font-medium flex flex-row border-gray-800 bg-gray-800 hover:bg-gray-900 px-2"
-            onClick={() => router.push("/watchlist")}
+            variants='text'
+            classes='font-medium flex flex-row border-gray-800 bg-gray-800 hover:bg-gray-900 px-2'
+            onClick={() => router.push('/watchlist')}
           >
-            <AiOutlineShoppingCart color="white" size={20} />
+            <AiOutlineShoppingCart color='white' size={20} />
           </Button>
         </section>
       )}
     </header>
-  );
+  )
 }
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,46 +1,46 @@
-import useFetch from "@utils/hooks/useFetch";
-import Head from "next/head";
-import Listing from "@modules/Listing";
-import { useRouter } from "next/router";
-import { NextPageContext } from "next";
-import { API } from "@utils/assets/constants/routes";
+import useFetch from '@utils/hooks/useFetch'
+import Head from 'next/head'
+import Listing from '@modules/Listing'
+import { useRouter } from 'next/router'
+import { NextPageContext } from 'next'
+import { API } from '@utils/assets/constants/routes'
 
 export async function getServerSideProps(ctx: NextPageContext) {
-  const res = await fetch(`${API}/listings/search?query=${ctx.query.q}`);
-  const data = await res.json();
+  const res = await fetch(`${API}/listings/search?query=${ctx.query.q}`)
+  const data = await res.json()
 
-  console.log(data);
+  console.log(data)
 
   return {
     props: {
       query: { q: ctx.query.q },
       data: data ?? [],
     },
-  };
+  }
 }
 
 export default function Search({
   query,
   data,
 }: {
-  query: any;
-  data: ListingProps[];
+  query: any
+  data: ListingProps[]
 }) {
   return (
-    <main className="p-2 flex flex-col items-center">
+    <main className='p-2 flex flex-col items-center'>
       <Head>
         <title>Search</title>
       </Head>
 
-      <h1 className="text-white text-center text-5xl md:text-8xl font-bold mb-5">
+      <h1 className='text-white text-center text-rose-600 text-5xl md:text-7xl font-bold mb-5'>
         Matching text: {query.q}
       </h1>
 
-      <div className="p-2 w-full grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4 xl:w-2/3">
+      <div className='p-2 w-full grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 lg:grid-cols-4 lg:gap-4 xl:w-2/3'>
         {data.map((listing) => (
           <Listing key={listing.listing_id} {...listing} />
         ))}
       </div>
     </main>
-  );
+  )
 }
