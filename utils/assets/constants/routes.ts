c17f029d1dedcfc870323b8ebc6576da5d25a1ac
DMQQ <kozakdamian73@gmail.com>
2024-03-20 15:29:56 +0100

    fix of previous fix XD!

--- utils/assets/constants/routes.ts
+++ utils/assets/constants/routes.ts
@@ -1,4 +1,4 @@
-export const API = process.env.BACKEND_URL || "http://srv25.mikr.us:20163";
-
-export const API_CLIENT =
+export const API =
   process.env.NEXT_PUBLIC_BACKEND_URL || "http://srv25.mikr.us:20163";
+export const API_SERVER =
+  process.env.BACKEND_URL || "http://srv25.mikr.us:20163";
